eaadff86c842e3c8097f360ff1fd98c130528f88
GibsonSWE <jacob.henriksen94@gmail.com>
2023-04-14 19:20:10 +0200

    Started implementing resource check and payment handling

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+machine_running = True
+
+machine = CoffeeMaker
+current_menu = Menu
+money_machine = MoneyMachine
+
+while machine_running == True:
+    machine_input = current_menu.find_drink(input('What would you like? (espresso/latte/cappuccino): '))
+
+    if machine_input == 'off':
+        print('Stopping Machine')
+        machine_running = False
+    elif machine_input == 'report':
+        machine.report()
+    else:
+        if machine.is_resource_sufficient(machine_input) == True:
+            if money_machine.make_payment(money_machine) == True:
+                machine.make_coffee(current_menu.find_drink(machine_input))
