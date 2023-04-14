723d4a202915b46cee8df5724bb9b1ee1a66e8d2
GibsonSWE <jacob.henriksen94@gmail.com>
2023-04-14 20:10:53 +0200

    Program working according to instructions

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+machine = CoffeeMaker()
+current_menu = Menu()
+money_module = MoneyMachine()
+
 machine_running = True
-
-machine = CoffeeMaker
-current_menu = Menu
-money_machine = MoneyMachine
-
-while machine_running == True:
-    machine_input = current_menu.find_drink(input('What would you like? (espresso/latte/cappuccino): '))
+while machine_running:
+    machine_input = input(f'What would you like? ({current_menu.get_items()}): ')
 
     if machine_input == 'off':
         print('Stopping Machine')
         machine_running = False
     elif machine_input == 'report':
+        money_module.report()
         machine.report()
     else:
-        if machine.is_resource_sufficient(machine_input) == True:
-            if money_machine.make_payment(money_machine) == True:
-                machine.make_coffee(current_menu.find_drink(machine_input))
+        drink = current_menu.find_drink(machine_input)
+        if machine.is_resource_sufficient(drink):
+            if money_module.make_payment(drink.cost):
+                machine.make_coffee(drink)
